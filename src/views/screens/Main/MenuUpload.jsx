632f81ce0e0b1f60703b11e64614ffb6eb3b366b
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-07-05 23:31:11 +0700

    render upload data

--- src/views/screens/Main/MenuUpload.jsx
+++ src/views/screens/Main/MenuUpload.jsx
@@ -3,12 +3,14 @@ import "./Main.css";
 import InputUI from "../../components/Input/Input";
 import ButtonUI from "../../components/Button/Button";
 import { ExcelRenderer, OutTable } from "react-excel-renderer";
+import Table from "react-bootstrap/Table";
 
 class MenuUpload extends Component {
   state = {
     selectedFile: null,
-    cols : [] ,
-    rows :[]
+    cols: [],
+    rows: [],
+    data: [],
   };
 
   // fileUploadHandler = (e) => {
@@ -18,25 +20,49 @@ class MenuUpload extends Component {
 
   fileUploadHandler = (event) => {
     let fileObj = event.target.files[0];
-    
+
     //just pass the fileObj as parameter
     ExcelRenderer(fileObj, (err, resp) => {
-      if(err){
-        console.log(err);            
-      }
-      else{
-        console.log("err :"+err);
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log("err :" + err);
 
-        console.log(resp);
-        console.log(resp.cols);
+        // console.log(resp);
+        // console.log(resp.cols);
         this.setState({
-          cols: resp.cols,
-          rows: resp.rows
+          // cols: resp.cols,
+          // rows: resp.rows,
+          data: resp.rows,
         });
       }
-    });               
-    
-    }
+    });
+  };
+
+  renderUploadData = () => {
+    return this.state.data.map((val, idx) => {
+      if (idx === 0) {
+        return (
+          <thead>
+            <tr>
+              {val.map((val) => {
+                return <td>{val}</td>;
+              })}
+            </tr>
+          </thead>
+        );
+      } else {
+        return (
+          <tr>
+            {val.map((val) => {
+              return <td>{val}</td>;
+            })}
+          </tr>
+        );
+      }
+    });
+  };
+
   render() {
     return (
       <>
@@ -51,7 +77,7 @@ class MenuUpload extends Component {
                 <div className="custom-file">
                   <input
                     // value ={this.state.selectedFile}
-                    
+
                     onChange={(e) => this.fileUploadHandler(e)}
                     type="file"
                     className="custom-file-input"
@@ -70,12 +96,16 @@ class MenuUpload extends Component {
               </div>
             </div>
           </div>
-          <OutTable
+          {/* <OutTable
             data={this.state.rows}
             columns={this.state.cols}
             tableClassName="ExcelTable2007"
             tableHeaderRowClass="heading"
-          />
+          /> */}
+          <Table striped bordered hover responsive>
+            <thead>{this.renderUploadData()}</thead>
+            <tbody>{this.renderUploadData()}</tbody>
+          </Table>
           <div className="d-flex justify-content-center align-items-center">
             <ButtonUI className="m-3">Upload</ButtonUI>
             <ButtonUI type="outline" className="m-3">
